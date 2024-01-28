cd5741654161b0d84faa5b4d01f37c5d04ad0e64
DaniilCheban <danyacheban@icloud.com>
2024-01-28 15:11:01 +0100

    hw

--- src/components/API.js
+++ src/components/API.js
@@ -5,6 +5,6 @@ const BASE_URL = 'https://pixabay.com/api';
 
 export const fetchGalleryItems = async (searchText, pages, per_page) => {
   return await axios.get(
-    `${BASE_URL}/?q=${searchText}&key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${pages}`
+    `${BASE_URL}/?q=${searchText}&key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}=12&page=${pages}`
   );
 };
